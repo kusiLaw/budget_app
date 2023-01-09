5045a5869d7570265c38b8e94d93c74cd17a583d
kusi <lawcubegsm@gmail.com>
2023-01-09 18:15:16 +0000

    Add splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- root "homes#splash"
+  root "homes#splash"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
